15174ce7fbcc0b65afa2b9e1828a0d8fc4f094ae
traderbastienfaure <traderbastienfaure@gmail.com>
2026-04-21 13:32:33 +0200

    removed react

--- src/components/RecipeCard.tsx
+++
@@ -1,7 +0,0 @@
-import type { CollectionEntry } from "astro:content";
-
-const RecipeCard = ({ recipe }: { recipe: CollectionEntry<"recipes"> }) => {
-  return <a href={`recipes/${recipe.id}`}>{recipe.data.title}</a>;
-};
-
-export default RecipeCard;
